2bd0820ee5a548de796ae828f4ba23daeda5551b
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-06-03 17:32:39 +0530

    Read json file from filters.json

--- src/load_filter.py
+++ src/load_filter.py
@@ -1,12 +1,7 @@
 import json
 
-def load(filters):
-    try:
-        with open(filters, "r") as file:
-            data = json.load(file)
-    except FileNotFoundError:
-        print("The filter.json file is not present")
-        return {
-            "extensions" : [],
-            "files" : []
-        }
+with open("filters.json") as file:
+    data = file.read()
+
+filters = json.loads(data)
+print(filters)
