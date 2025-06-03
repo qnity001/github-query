bba6764820bfeeb24f93d3e842432767b4774e7d
martandlifecare <martandlifecare@gmail.com>
2025-06-03 11:28:51 +0530

    Check invalidity of input path from user

--- repo_tree.py
+++ repo_tree.py
@@ -1,3 +1,10 @@
+import os
 # later argparse
 
 folder_path = input("Enter the root directory path: ")
+
+if not os.path.isdir(folder_path):
+    print("Invalid folder path. Please try again.")
+    exit()
+
+
