c98e04d15994b0151c07d354537d5fe6edebe440
martandlifecare <martandlifecare@gmail.com>
2025-06-03 11:34:13 +0530

    Write code to verify and test if input path is correct

--- repo_tree.py
+++ repo_tree.py
@@ -1,10 +1,12 @@
 import os
-# later argparse
+import sys
 
+# later argparse
 folder_path = input("Enter the root directory path: ")
 
 if not os.path.isdir(folder_path):
     print("Invalid folder path. Please try again.")
-    exit()
+    sys.exit(1)
 
-
+else:
+    print("The input is correct")
