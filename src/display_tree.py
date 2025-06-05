12ddb7a617c8be5de77a61b643d77dc70028569e
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-06-05 11:25:24 +0530

    Write a program to display the tree
    - Remove the clutter from the repo_tree.py program
    - Abstract the display tree function to write clean code

---
+++ src/display_tree.py
@@ -0,0 +1,17 @@
+# This program inputs a dictionary and displays it in a tree form
+
+from rich.tree import Tree
+from rich.console import Console
+
+def display(tree: dict, print = None):
+    # Print the root
+    if print is None:
+        print = Tree(f"[bold]Root Directory")
+
+    for name, subtree in tree.items():
+        if isinstance(subtree, dict):
+            new = print.add(f"[blue]{name}")
+            display(subtree, new)
+        else:
+            print.add(name)
+    return print
