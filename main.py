d099f3eeffadf07d59f89bb6fe4ded6a4b55d0f0
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-06-12 14:00:26 +0530

    Run chunking from main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.return_path import save_directory
 from src.tree import repo_tree
+from src.chunking import chunking
 import json
 
 user_input = input("Enter the root directory path or GitHub link: ")
@@ -9,3 +10,4 @@ with open("data/outputs/meta.json", "w") as file:
     json.dump({"folder_path": str(folder_path)}, file)
 
 repo_tree.run()
+chunking.run()
