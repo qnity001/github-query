2d923a942c69cebbd0758015216c78c732006f2f
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-06-12 13:29:14 +0530

    Run repo_tree.py from main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from src.return_path import save_directory
+from src.tree import repo_tree
 import json
 
 user_input = input("Enter the root directory path or GitHub link: ")
 folder_path = save_directory(user_input)
 
 with open("data/outputs/meta.json", "w") as file:
-    json.dump({"repo_path": str(folder_path)}, file)
+    json.dump({"folder_path": str(folder_path)}, file)
+
+repo_tree.run()
