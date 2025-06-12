c335416a317a7bd43cc9d2a1e3f6102355dd2a32
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-06-12 15:40:16 +0530

    Split files based on priority flag and save path

--- src/chunking/chunking.py
+++ src/chunking/chunking.py
@@ -1,9 +1,28 @@
 from src.config import get_repo_path
 from pathlib import Path
+import json
 
-repo_root = get_repo_path
+priority = []
+non_priority = []
 
+def get_tree():
+    with open("data/outputs/repo_tree.json", "r") as file:
+        return json.load(file)
+    
+def split_files(tree: dict):
+    for name, meta in tree.items():
+        if meta["type"] == "folder":
+            split_files(meta["children"])
+        elif meta["type"] == "file":
+            if meta["priority"] == "True":
+                priority.append(Path(meta["path"]))
+            else:
+                non_priority.append(Path(meta["path"]))
 
 
 def run():
-    return
+    repo_root = get_repo_path()
+    tree = get_tree()
+    split_files(tree)
+    print(priority)
+    print(non_priority)
