3534b26be2b96306437ff318d4af2446b561159e
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-06-16 12:59:00 +0530

    Import autotokenizer and return the number of tokens in each file

--- src/chunking/chunking.py
+++ src/chunking/chunking.py
@@ -1,13 +1,16 @@
 from src.config import get_repo_path
 from pathlib import Path
 import json
+from transformers import AutoTokenizer
 
 priority = []
 non_priority = []
+test_chunks = []
+tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/deepseek-coder-6.7b-instruct")
 
 def get_tree():
-    with open("data/outputs/repo_tree.json", "r") as file:
-        return json.load(file)
+    with open("data/outputs/repo_tree.json", "r") as fileread:
+        return json.load(fileread)
     
 def split_files(tree: dict):
     for name, meta in tree.items():
@@ -19,10 +22,18 @@ def split_files(tree: dict):
             else:
                 non_priority.append(Path(meta["path"]))
 
+def process_files(files: list, root):
+    for file_path in files:
+        path = root / file_path
+        with open(path, "r", encoding="utf-8") as fileread:
+            content = fileread.read()
+            tokens = tokenizer.encode(content, truncation = False)
+            print(f"{path} = {len(tokens)}")
 
 def run():
-    repo_root = get_repo_path()
+    repo_root = Path(get_repo_path())
     tree = get_tree()
     split_files(tree)
     print(priority)
     print(non_priority)
+    process_files(priority, repo_root)
