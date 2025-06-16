203035e7b605be0e63a59fe9f547b04f9b62f110
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-06-16 13:59:05 +0530

    Input path of repo through CLI

--- main.py
+++ main.py
@@ -2,12 +2,13 @@ from src.return_path import save_directory
 from src.tree import repo_tree
 from src.chunking import chunking
 import json
+import sys
 
-user_input = input("Enter the root directory path or GitHub link: ")
+user_input = sys.argv[1]
 folder_path = save_directory(user_input)
 
 with open("data/outputs/meta.json", "w") as file:
     json.dump({"folder_path": str(folder_path)}, file)
 
 repo_tree.run()
-chunking.run()
+#chunking.run()
