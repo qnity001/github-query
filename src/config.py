fc5193474e658523959b605e3234331e1a29fc75
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-06-17 11:54:27 +0530

    Define path for chunks output

--- src/config.py
+++ src/config.py
@@ -2,6 +2,7 @@ import json
 from pathlib import Path
 
 data_dir = Path(__file__).parent.parent / "data"
+chunks_path = data_dir / "outputs" / "chunk_output.json"
 
 def get_repo_path():
     with open(data_dir / "outputs/meta.json", "r") as file:
