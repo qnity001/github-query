fa25251000885abc2d298a1ea51e657568e525d7
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-06-17 11:39:37 +0530

    Create path to data directory

--- src/config.py
+++ src/config.py
@@ -1,7 +1,9 @@
 import json
 from pathlib import Path
 
+data_dir = Path(__file__).parent.parent / "data"
+
 def get_repo_path():
-    with open(Path(__file__).parent.parent / "data/outputs/meta.json", "r") as file:
+    with open(data_dir / "outputs/meta.json", "r") as file:
         meta = json.load(file)
     return Path(meta["folder_path"])
