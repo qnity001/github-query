14407aac407be8ccefd2f7242ca0f30fe6ac2325
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-06-18 20:47:16 +0530

    Get lists from controller.py

---
+++ src/chunking/processor.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+from src.config import get_repo_path
+from src.chunking.controller import return_list
+    
+def read_and_tokenize(file_list: list, priority: bool, repo_root):
+    
+    return
+
+if __name__ == "__main__":
+    repo_root = get_repo_path()
+    priority, non_priority = return_list()
