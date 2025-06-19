d1f4834b84ae5590fbb1013cc1792924612b918a
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-06-19 16:06:15 +0530

    Take input file lists from controller and run from main

--- src/chunking/processor.py
+++ src/chunking/processor.py
@@ -1,11 +1,27 @@
 from pathlib import Path
-from src.config import get_repo_path
 from src.chunking.controller import return_list
-    
-def read_and_tokenize(file_list: list, priority: bool, repo_root):
-    
-    return
+import json
+from transformers import AutoTokenizer
+#from src.config import get_repo_path
 
-if __name__ == "__main__":
-    repo_root = get_repo_path()
-    priority, non_priority = return_list()
+tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/deepseek-coder-6.7b-instruct")
+
+def read_and_chunk(file_list: list, priority: bool, repo_root):
+    for file_path in file_list:
+        chunk_number = -1
+        path = repo_root / file_path
+        with open(path, "r", encoding="utf-8") as file:
+            content = file.read()
+        chunk = {
+            "chunk_id": f"{file_path}::{chunk_number + 1}",
+            "file_path": path,
+            "content": content,
+            "token_count": len(tokenizer.encode(content)),
+            "priority": priority
+        }
+        print(chunk)
+    
+def run():
+    #repo_root = get_repo_path()
+    files = return_list()
+    print(files)
