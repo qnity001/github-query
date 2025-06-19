d725f7341967d4ebec5ebbdf067ef5444e9d69f5
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-06-19 17:07:45 +0530

    Run processor.py to load the chunks in jsonl

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 from src.return_path import save_directory
 from src.tree import repo_tree
-from src.chunking import chunking
+from src.chunking import processor
 import json
-import sys
 
-user_input = sys.argv[1]
+user_input = input("Please input:")
 folder_path = save_directory(user_input)
 
 with open("data/outputs/meta.json", "w") as file:
     json.dump({"folder_path": str(folder_path)}, file)
 
 repo_tree.run()
-chunking.run()
+processor.run()
