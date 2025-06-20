db30e0824d4a135a1e8954748653258accf9582c
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-06-20 12:07:12 +0530

    Apply cosine distancing to FAISS index

--- src/embedding/embedding.py
+++ src/embedding/embedding.py
@@ -5,6 +5,7 @@ import json
 from sentence_transformers import SentenceTransformer
 import faiss
 import numpy
+from sklearn.preprocessing import normalize
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
@@ -17,11 +18,12 @@ def embed():
     chunks = get_chunks()
     texts = [chunk["content"] for chunk in chunks]
     vectors = model.encode(texts, show_progress_bar = True) # Vectors from all the chunks
+    normalized_vectors = normalize(numpy.array(vectors), axis = 1)
     
     # Store in FAISS Database as index
-    dimension = vectors[0].shape[0]
-    index = faiss.IndexFlatL2(dimension)
-    index.add(numpy.array(vectors))
+    dimension = normalized_vectors.shape[1]
+    index = faiss.IndexFlatIP(dimension)
+    index.add(normalized_vectors)
     faiss.write_index(index, "../../data/outputs/faiss.index")
 
 embed()
