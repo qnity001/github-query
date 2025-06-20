a23665cec960f8a4db11958d3baffe78a377976a
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-06-20 11:48:58 +0530

    Create vectors from all chunks

--- src/embedding/embedding.py
+++ src/embedding/embedding.py
@@ -3,13 +3,18 @@
 
 import json
 from sentence_transformers import SentenceTransformer
+import faiss
+import numpy
 
-#model = SentenceTransformer("all-MiniLM-L6-v2")
+model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def get_chunks():
     chunks_path = "../../data/outputs/chunks.json"
     with open(chunks_path) as file:
         return json.load(file)
-
-chunks = get_chunks()
-print(chunks)
+    
+def embed():
+    chunks = get_chunks()
+    texts = [chunk["content"] for chunk in chunks]
+    vectors = model.encode(texts, show_progress_bar = True) # Vectors from all the chunks
+    print(vectors)
