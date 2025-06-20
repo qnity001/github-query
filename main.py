1e162a24ba884b6d288c7af4a9349b01aa301dc9
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-06-20 12:54:28 +0530

    Run embedding.py from main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from src.return_path import save_directory
 from src.tree import repo_tree
 from src.chunking import processor
+from src.embedding import embedding
 import json
+import sys
 
-user_input = input("Please input:")
+user_input = sys.argv[1]
 folder_path = save_directory(user_input)
 
 with open("data/outputs/meta.json", "w") as file:
@@ -11,3 +13,4 @@ with open("data/outputs/meta.json", "w") as file:
 
 repo_tree.run()
 processor.run()
+embedding.embed_and_store()
--- src/embedding/embedding.py
+++ src/embedding/embedding.py
@@ -10,11 +10,11 @@ from sklearn.preprocessing import normalize
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
 def get_chunks():
-    chunks_path = "../../data/outputs/chunks.json"
+    chunks_path = "data/outputs/chunks.json"
     with open(chunks_path) as file:
         return json.load(file)
     
-def embed():
+def embed_and_store():
     chunks = get_chunks()
     texts = [chunk["content"] for chunk in chunks]
     vectors = model.encode(texts, show_progress_bar = True) # Vectors from all the chunks
@@ -24,6 +24,4 @@ def embed():
     dimension = normalized_vectors.shape[1]
     index = faiss.IndexFlatIP(dimension)
     index.add(normalized_vectors)
-    faiss.write_index(index, "../../data/outputs/faiss.index")
-
-embed()
+    faiss.write_index(index, "data/outputs/faiss.index")
