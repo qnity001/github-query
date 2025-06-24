dfae89bea84685094b92dcd257623ca72fa0b994
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-06-24 16:22:04 +0530

    Add name based and chunk based searching to return path

--- src/retrieving/retriever.py
+++ src/retrieving/retriever.py
@@ -8,19 +8,34 @@ def get_chunks():
     with open(chunks_path) as file:
         return json.load(file)
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+def get_names():
+    names_path = "../../data/outputs/name_chunks.json"
+    with open(names_path) as file:
+        return json.load(file)
+    
+# Search name index in FAISS
+def search_names(query_vector):
+    names = get_names()
+    name_index = faiss.read_index("../../data/outputs/faiss_names.index")
+    k = 2
+    D, I = name_index.search(numpy.array(query_vector), k)
+    top_chunks = [names[i] for i in I[0]]
+    for chunk in top_chunks:
+       print(chunk["path"])
 
-user_query = input("Ask your query from codebase: ")
-query_vector = model.encode([user_query], normalize_embeddings = True)
+# Search chunks semantically in FAISS
+def search_chunks(query_vector):
+    chunks = get_chunks()
+    name_index = faiss.read_index("../../data/outputs/faiss_chunks.index")
+    k = 2
+    D, I = name_index.search(numpy.array(query_vector), k)
+    top_chunks = [chunks[i] for i in I[0]]
+    for chunk in top_chunks:
+        print(chunk["file_path"])
 
-chunks = get_chunks()
-
-# Search FAISS
-index = faiss.read_index("../../data/outputs/faiss.index")
-k = 2
-D, I = index.search(numpy.array(query_vector), k)
-
-# Print top chunks
-top_chunks = [chunks[i] for i in I[0]]
-for chunk in top_chunks:
-    print(chunk["file_path"])
+def run():
+    model = SentenceTransformer("all-MiniLM-L6-v2") 
+    user_query = input("Ask your query from codebase: ")
+    query_vector = model.encode([user_query], normalize_embeddings = True)
+    search_names(query_vector)
+    search_chunks(query_vector)
