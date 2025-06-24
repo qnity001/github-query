a867d8fa6b447f36c798dc706b61359537529339
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-06-24 16:37:51 +0530

    Increase 'k' and remove duplicates from both searches

--- src/retrieving/retriever.py
+++ src/retrieving/retriever.py
@@ -4,38 +4,43 @@ from sentence_transformers import SentenceTransformer
 import numpy
 
 def get_chunks():
-    chunks_path = "../../data/outputs/chunks.json"
+    chunks_path = "data/outputs/chunks.json"
     with open(chunks_path) as file:
         return json.load(file)
 
 def get_names():
-    names_path = "../../data/outputs/name_chunks.json"
+    names_path = "data/outputs/name_chunks.json"
     with open(names_path) as file:
         return json.load(file)
     
 # Search name index in FAISS
 def search_names(query_vector):
+    paths = []
     names = get_names()
-    name_index = faiss.read_index("../../data/outputs/faiss_names.index")
-    k = 2
+    name_index = faiss.read_index("data/outputs/faiss_names.index")
+    k = 6
     D, I = name_index.search(numpy.array(query_vector), k)
     top_chunks = [names[i] for i in I[0]]
     for chunk in top_chunks:
-       print(chunk["path"])
+       paths.append(chunk["path"])
+    return paths
 
 # Search chunks semantically in FAISS
 def search_chunks(query_vector):
+    paths = []
     chunks = get_chunks()
-    name_index = faiss.read_index("../../data/outputs/faiss_chunks.index")
-    k = 2
+    name_index = faiss.read_index("data/outputs/faiss_chunks.index")
+    k = 6
     D, I = name_index.search(numpy.array(query_vector), k)
     top_chunks = [chunks[i] for i in I[0]]
     for chunk in top_chunks:
-        print(chunk["file_path"])
+        paths.append(chunk["file_path"])
+    return paths
 
 def run():
     model = SentenceTransformer("all-MiniLM-L6-v2") 
     user_query = input("Ask your query from codebase: ")
     query_vector = model.encode([user_query], normalize_embeddings = True)
-    search_names(query_vector)
-    search_chunks(query_vector)
+    paths = list(set(search_names(query_vector) + search_chunks(query_vector)))
+    for path in paths:
+        print(path)
