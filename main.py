19894e3b501f9e447953d9f7500a4de670b44a9c
Aarya Aishwaryam <aishwaryam.aarya@gmail.com>
2025-07-18 15:16:39 +0530

    Create function to run the main program

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
-"""
-from src.return_path import delete_link_repo
+#'''
+import os
+import json
+from src.tree import create_mermaid, repo_tree
 from src.return_path import save_directory
-from src.tree import repo_tree, create_mermaid
 from src.chunking import processor
 from src.embedding import embedding
 from src.llm_query import chatbot
-import json
-import sys
-import os
 
-user_input = sys.argv[1]
-folder_path = save_directory(user_input)
+def run(user_input):
+    folder_path = save_directory(user_input)
 
-os.makedirs("data/outputs", exist_ok = True)
-with open("data/outputs/meta.json", "w") as file:
-    json.dump({"folder_path": str(folder_path)}, file)
+    os.makedirs("data/outputs", exist_ok = True)
+    with open("data/outputs/meta.json", "w") as file:
+        json.dump({"folder_path": str(folder_path)}, file)
 
-repo_tree.run()
-processor.run()
-embedding.run()
-chatbot.run()
-delete_link_repo(user_input, folder_path)
-"""
-
-from src.tree import create_mermaid
-create_mermaid.run()
+    repo_tree.run()
+    create_mermaid.run()
+    processor.run()
+    embedding.run()
+    #chatbot.run()
+#'''
